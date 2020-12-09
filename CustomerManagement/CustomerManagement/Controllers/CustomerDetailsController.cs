309034fbf2e801271b735a78e4d287779f2df9cf
Melissa Holmes <melholmes3195@gmail.com>
2020-12-09 21:54:01 +0000

    Adding a Controller called CustomerDetailsController

---
+++ CustomerManagement/CustomerManagement/Controllers/CustomerDetailsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerManagement.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerDetailsController : ControllerBase
+    {
+        private readonly CustomerDetailsDbConnection _context;
+
+        public CustomerDetailsController(CustomerDetailsDbConnection context)
+        {
+            _context = context;
+        }
+    }
+}
