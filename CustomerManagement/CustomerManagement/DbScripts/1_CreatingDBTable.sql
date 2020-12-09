353c47a725dab031327b346b91ebe86c7cec1cbd
Melissa Holmes <melholmes3195@gmail.com>
2020-12-09 23:22:42 +0000

    Adding DB scripts that create table and add a row of data

---
+++ CustomerManagement/CustomerManagement/DbScripts/1_CreatingDBTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE CustomerData (
+    ID int,
+	Title varchar(20),
+    FirstName varchar(50),
+	LastName varchar(50),
+	EmailAddress varchar(75),
+	Mobile varchar(15),
+    AddressLine1 varchar(80),
+    AddressLine2 varchar(80),
+    Town varchar(50),
+    County varchar(50),
+    City varchar(50),
+    PostCode varchar(50), 
+	Country varchar(50), 
+	IsActive bit
+);
---
+++ CustomerManagement/CustomerManagement/DbScripts/2_Adding row of data.sql
@@ -0,0 +1,13 @@
+INSERT INTO CustomerData (Title
+,FirstName
+,LastName
+,EmailAddress
+,Mobile
+,AddressLine1
+,AddressLine2
+,Town
+,County
+,City
+,PostCode
+,Country)
+ VALUES('miss', 'Melissa', 'Holmes', 'melholmes3195@gmail.com', '09888989898', 'flat 7', 'road', 'manchester', 'GM', 'GM', 'M765HG', 'UK')
