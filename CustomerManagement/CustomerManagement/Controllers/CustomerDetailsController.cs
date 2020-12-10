7df37deb49af83f184515a637f5a5ac37f5fc8cd
Melissa Holmes <melholmes3195@gmail.com>
2020-12-10 00:05:31 +0000

    Adding a Controller and attempts at HTTP protocol

--- CustomerManagement/CustomerManagement/Controllers/CustomerDetailsController.cs
+++ CustomerManagement/CustomerManagement/Controllers/CustomerDetailsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CustomerManagement.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,6 +19,61 @@ namespace CustomerManagement.Controllers
         public CustomerDetailsController(CustomerDetailsDbConnection context)
         {
             _context = context;
+        }
+        //GET:api/CustomerDetails
+        [HttpGet]
+        public ActionResult<IEnumerable<CustomerDetails>> GetCommands()
+        {
+            return _context.CustomerDetails;
+        }
+
+        //GET: api/CustomerDetails/IsActive
+        [HttpGet("{IsActive}")]
+        public ActionResult<CustomerDetails> GetCommandItem(bool IsActive)
+        {
+            var itm = _context.CustomerDetails.Find(IsActive);
+            if (itm == null)
+            {
+                return NotFound();
+            }
+            return itm;
+        }
+
+        //POST: api/CustomerDetails
+        [HttpPost]
+        public ActionResult<CustomerDetails> PostCommandItem(CustomerDetails details)
+        {
+            _context.CustomerDetails.Add(details);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetCommandItem", new CustomerDetails { Id = details.Id }, details);
+        }
+
+        //PUT api/CustomerDetails/n
+        [HttpPut("{IsActive}")]
+        public ActionResult PutCommandItem(int IsActive, CustomerDetails details)
+        {
+            if (IsActive.Equals("True"))
+            {
+                _context.Entry(details).State = EntityState.Modified;
+                _context.SaveChanges();             
+            }
+            return NoContent();
+        }
+
+        //DELETE api/CustomerDetails/n
+        [HttpDelete("{CustomerDetails}")]
+        public ActionResult<CustomerDetails> DeleteDetailsItem(int id, CustomerDetails details)
+        {
+            var itm = _context.CustomerDetails.Find(id);
+            if (itm == null)
+            {
+                return NotFound();
+            }
+            _context.CustomerDetails.Remove(details);
+            _context.SaveChanges();
+
+            return details;
         }
     }
 }
--- CustomerManagement/CustomerManagement/Startup.cs
+++ CustomerManagement/CustomerManagement/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CustomerManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
